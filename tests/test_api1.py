332cd07500bd7e77f50816bf91f4f1366b05faa6
MoAb5993 <isdead454@gmail.com>
2025-05-05 14:01:32 +0300

    main1.py -> api.py

--- tests/test_api1.py
+++ tests/test_api1.py
@@ -3,13 +3,15 @@
 import unittest
 from fastapi.testclient import TestClient
 
-from main1 import app
+from api import app
 
 client = TestClient(app)
 
 
 class TestAPI(unittest.TestCase):
     """Класс тестирования API"""
-    response = client.get("/solve?a=0&b=0&c=5")
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Уравнение не имеет решений"}
+    def test_c_not_equal_zero():
+        """Тестирование a=0, b=0, c!=0"""
+        response = client.get("/solve?a=0&b=0&c=5")
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Уравнение не имеет решений"}
