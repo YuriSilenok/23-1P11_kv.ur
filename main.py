220e10a0d30d1feeaeb79ae61421bec57278f69c
Bhdusuhs <147725400+Bhdusuhs@users.noreply.github.com>
2025-05-05 13:58:13 +0300

    Def a=0 b=0 c!=0 Альбов

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ def treesome(a, b, c):
     """Функция для вычисления корней квадратного уровнения"""
     if a == 0:
         return ('Прямая будет расположена поралельно оси x')
-    else:
-        d = b**2-4*(a*c)
+    d = b**2-4*(a*c)
     if d > 0:
         return d, ((-b - d ** (0.5)) / (a * 2)), ((-b + d ** (0.5)) / (a * 2))
     if d == 0:
         return d, (-b / (a * 2))
-
     return d, "Корней нет"
 
 
@@ -20,6 +18,3 @@ if __name__ == '__main__':
     B = float(input('Введите b: '))
     C = float(input('Введите c: '))
     print(treesome(A, B, C))
-    
-
-    
