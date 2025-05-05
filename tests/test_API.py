c744e12db441ba59c8aab45a43b8f63dccd63819
MoAb5993 <isdead454@gmail.com>
2025-05-05 12:03:30 +0300

    Тестирование API a=0, b=0, c!=0

---
+++ tests/test_API.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+    
+
+def test_с_not_equal_zero():
+    # Тест: a=0, b=0, c≠0 → Уравнение не имеет решений
+    response = client.get("/solve?a=0&b=0&c=5")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Уравнение не имеет решений"}
+
