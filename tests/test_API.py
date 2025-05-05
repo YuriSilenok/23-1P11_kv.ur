61c232df38e8bc1ad7003c6e67193c3de139a98a
MoAb5993 <isdead454@gmail.com>
2025-05-05 13:16:18 +0300

    Использование библиотеки unittest

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,12 +1,13 @@
 from fastapi.testclient import TestClient
+import unittest
 
 from .main import app
 
 client = TestClient(app)
 
 
-def test_с_not_equal_zero():
-    """Тест: a=0, b=0, c≠0 Уравнение не имеет решений"""
+class TestAPI(unittest.TestCase):
+    """Класс тестирования API"""
     response = client.get("/solve?a=0&b=0&c=5")
     assert response.status_code == 400
     assert response.json() == {"detail": "Уравнение не имеет решений"}
